826a7d81bcf778acdaf94273071fd202d3a862e8
Esther Kondowe <kondoweesther2@gmail.com>
2026-05-03 07:40:27 +0200

    correct CORS to cors

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,12 @@ require('dotenv').config();
 const connectToDb= require('./config/db_connection');
 const feedbackRoutes= require('./routes/feedbackRoute')
 const adminRoutes= require('./routes/adminRoute')
-const CORS= require('cors')
+const cors= require('cors')
 const app= express();
 
 
  app.use(express.json());
- app.use(CORS({origin: ['http://localhost:3001','https://blossom-voices.netlify.app']}));
+ app.use(cors({origin: ['http://localhost:3001','https://blossom-voices.netlify.app']}));
  
  //routes
  app.use('/api/feedback', feedbackRoutes);
