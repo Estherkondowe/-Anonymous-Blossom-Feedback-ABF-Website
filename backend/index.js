6c5740b0d5f293517de72a826ead2bef71c159ad
Esther Kondowe <kondoweesther2@gmail.com>
2026-05-03 07:38:06 +0200

    correct CORS

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app= express();
 
 
  app.use(express.json());
- app.use(cors({origin: ['http://localhost:3001','https://blossom-voices.netlify.app']}));
+ app.use(CORS({origin: ['http://localhost:3001','https://blossom-voices.netlify.app']}));
  
  //routes
  app.use('/api/feedback', feedbackRoutes);
