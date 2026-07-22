d68f7effdea30083bb75287c710a3b1c6998a6b6
Esther Kondowe <kondoweesther2@gmail.com>
2026-07-22 09:56:10 +0200

    rewrote the env file in backend

--- Frontend/blossom-feedback/src/pages/AdminLogin.jsx
+++ Frontend/blossom-feedback/src/pages/AdminLogin.jsx
@@ -8,16 +8,12 @@ function AdminLogin() {
                 <h1>🌸 Admin Login</h1>
                 <p>Sign in with your Code Blossom Google account</p>
             </div>
-
-                <a href='http://localhost:3000/api/auth/google' className='google-btn'>
-                    <img
-                        src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg"
-                        alt="Google"
-                        width="20"
-                    />
-                    Sign in with Google
-                </a>
-
+            <a href='https://anonymous-blossom-feedback-abf-website.onrender.com/api/auth/google' className='google-btn'>
+                <img
+                    src="https://www.gstatic.com/firebasejs/ui/2.0.0/images/auth/google.svg" alt="Google" width="20"
+                />
+                Sign in with Google
+            </a>
                 <p className='login-footer'>
                     For admins only. Participants don't need to login 🌸
                 </p>
